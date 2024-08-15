b39c6757c382170fadab3f06e0566fed92e55aed
tulsitailor <tulsitailor01@gmail.com>
2024-08-15 18:46:06 -0400

    Added about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
+import About from '@/components/About';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-400 to-purple-600">
-      <div className="text-center text-white">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-        <p className="text-lg">This is a simple starting page to test your Next.js setup.</p>
-        <button className="px-4 py-2 bg-pink-500 text-white rounded hover:bg-blue-600">
-          Get Started
-        </button>
-      </div>
-    </div>
+    <About />
   );
 }
 
---
+++ src/components/About.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { useState } from 'react';
+import { FaLinkedin, FaGithub, FaInstagram, FaEnvelope } from 'react-icons/fa';
+
+const About: React.FC = () => {
+    return (
+      <>
+        <section className="about-container" id="about-section" aria-labelledby="about-heading">
+          <header>
+            <h1 id="about-heading" className="name">TULSI TAILOR</h1>
+          </header>
+          <div className="about-content">
+            <h2 className="title">SOFTWARE ENGINEER & FULL-STACK DEVELOPER</h2>
+            <p className="introduction">
+              Passionate about building scalable web applications and always eager to learn new technologies.
+            </p>
+  
+            <div className="social-media-icons">
+              <a href="mailto:tulsitailor01@gmail.com" target="_blank" rel="noopener noreferrer">
+                <FaEnvelope />
+              </a>
+              <a href="https://www.linkedin.com/in/tulsi-tailor" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin />
+              </a>
+              <a href="https://github.com/ttulsi01" target="_blank" rel="noopener noreferrer">
+                <FaGithub />
+              </a>
+              <a href="https://www.instagram.com/tulsitailor_" target="_blank" rel="noopener noreferrer">
+                <FaInstagram />
+              </a>
+            </div>
+            <a
+              href="https://drive.google.com/file/d/1pG_pvkuenUcXAgp13W5Y4gUf2mwGPZ0Z/view?usp=sharing"
+              download
+              className="resume-button"
+              aria-label="Download Tulsi Tailor's Resume"
+            >
+              Download my Resume
+            </a>
+          </div>
+        </section>
+      </>
+    );
+  };
+  
+  export default About;
